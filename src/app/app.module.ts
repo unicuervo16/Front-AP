224a184b4c2ed2cf9cbd9bf5641eeb9694a8240a
Joel <joeplatiniuni16@gmail.com>
2022-03-06 18:51:24 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { BannerComponent } from './banner/banner.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LineaComponent } from './linea/linea.component';
+import { LoginComponent } from './LogIn/login.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { EducacionComponent } from './educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    SkillsComponent,
+    LineaComponent,
+    LoginComponent,
+    ProyectosComponent,
+    EducacionComponent,
   ],
   imports: [
     BrowserModule,
